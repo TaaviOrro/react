b28a2b9f7b4de5e4234e9c8a7e37a03ce8936c5e
Taavi <taavi.orro@voco.ee>
2025-01-23 14:30:18 +0200

    1.React projekti loomine ja kaivitamine

--- react-crash-2024/src/App.jsx
+++ react-crash-2024/src/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import react from 'react';
+import './App.css'
 import ExpenseItem from './components/ExpenseItem';
 
 const App = () => {
+  const expenses = [
+    {
+      date: new Date(2024, 10, 12),
+      title: 'New book',
+      price: 30.99
+    },
+    {
+      date: new Date(2024, 10, 12),
+      title: 'New jeans',
+      price: 99.99
+    }
+  ]
+
   return (
     <div className="App">
-      <ExpenseItem/>
+      <ExpenseItem data={expenses[0]}/>
+      <ExpenseItem data={expenses[1]}/>
     </div>
   )
 }
--- react-crash-2024/src/main.jsx
+++ react-crash-2024/src/main.jsx
@@ -1,12 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+root.render( 
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
