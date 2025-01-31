d482d7cbc3952d6133bf563fb0de0fc3899f5ad1
Taavi <taavi.orro@voco.ee>
2025-01-31 13:53:14 +0200

    8. ulesanne 3

--- react-crash-2024/src/components/ExpenseItem.jsx
+++ react-crash-2024/src/components/ExpenseItem.jsx
@@ -1,4 +1,5 @@
 import ExpenseDate from './ExpenseDate'
+import Card from './Card.jsx'
 import './ExpenseItem.css'
 import './ExpenseDate.css'
 
@@ -6,13 +7,13 @@ import './ExpenseDate.css'
 const ExpenseItem = (props) => {
 
     return (
-        <div className = 'expense-item'>
+        <Card className = 'expense-item'>
             <ExpenseDate date={props.data.date}/>
             <div className ='expense-item__description'>
                 <h2>{props.data.title}</h2>
                 <div className ='expense-item__price'>{props.data.price}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
